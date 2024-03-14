bdb4e353fe0ecaa650a0cd4ef83de39085ae0fc1
Arti <artiw362@gmail.com>
2024-03-14 17:22:46 +0530

    assigment uploaded

---
+++ src/Component/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h2 className="text">welcome to User</h2>
+            <img src="./images/user.jpg" width={"30%"}/>
+        </div>
+     );
+}
+ 
+export default Home;
